65468af11814a14ea204e84095646ca2788b4530
Lautaro <lautaro.kaza@gmail.com>
2025-11-25 16:51:56 -0300

    Remove .next folder

--- front/.next/dev/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__3cf52c28._.js")
-R.m("[project]/front/node_modules/next/app.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/front/node_modules/next/app.js [ssr] (ecmascript)").exports
--- front/src/app/HeroCarrousel.tsx
+++ front/src/app/HeroCarrousel.tsx
@@ -1,10 +1,23 @@
-export default function HeroCarrousel() {
+import { Movie } from "@/types/movie"
+
+export default function HeroCarrousel({
+    movies
+}: {
+    movies: Movie[]
+}) {
     return (
-        <div className="overflow-x-auto snap-x snap-mandatory flex gap-10 px-10 scroll-p-0 hide-scrollbar">
-            <div className="snap-center shrink-0 w-[1300px] max-w-full h-[560px] bg-blue-500 rounded-4xl"></div>
-            <div className="snap-center shrink-0 w-[1300px] max-w-full h-[560px] bg-green-500 rounded-4xl"></div>
-            <div className="snap-center shrink-0 w-[1300px] max-w-full h-[560px] bg-purple-500 rounded-4xl"></div>
-            <div className="snap-center shrink-0 w-[1300px] max-w-full h-[560px] bg-orange-500 rounded-4xl"></div>
+        <div className="relative max-w-[1300px] mx-auto">
+            <div className="overflow-x-auto snap-x snap-mandatory flex gap-10 px-10 scroll-p-0 hide-scrollbar">
+                <div className="snap-center shrink-0 w-[1300px] max-w-full h-[560px] bg-blue-500 rounded-4xl"></div>
+                <div className="snap-center shrink-0 w-[1300px] max-w-full h-[560px] bg-green-500 rounded-4xl"></div>
+                <div className="snap-center shrink-0 w-[1300px] max-w-full h-[560px] bg-purple-500 rounded-4xl"></div>
+                <div className="snap-center shrink-0 w-[1300px] max-w-full h-[560px] bg-orange-500 rounded-4xl"></div>
+            </div>
+            <div className="
+            absolute w-full h-full top-0 left-0
+            pointer-events-none
+            bg-[linear-gradient(90deg,_var(--background)_2%,_transparent_7%,_transparent_93%,_var(--background)_98%)]
+            "></div>
         </div>
     )
 }
