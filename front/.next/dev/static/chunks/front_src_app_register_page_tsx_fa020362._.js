a756be02d2baf6652650179a2c33935af34ec223
Lautaro <lautaro.kaza@gmail.com>
2025-11-25 16:58:35 -0300

    Remove .next folder

--- front/.next/dev/static/chunks/front_src_app_register_page_tsx_fa020362._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/front_src_app_register_RegisterForm_tsx_63b5d102._.js"
-],
-    source: "dynamic"
-});
