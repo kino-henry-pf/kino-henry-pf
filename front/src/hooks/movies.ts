7c6417aa4a7973c654d3408282982027fef926e2
Lautaro <lautaro.kaza@gmail.com>
2025-11-26 18:49:43 -0300

    Add movies getter hook

--- front/src/hooks/movies.ts
+++ front/src/hooks/movies.ts
@@ -1,12 +1,40 @@
 "use client"
 
 import { useEffect, useState } from "react"
+import { useApi } from "./api"
+import { Movie } from "@/types/movie"
 
-export const useMovies = () => {
+export const useMovies = ({
+    disableCache = true
+}: {
+    disableCache?: boolean
+}) => {
+    const api = useApi()
 
-    
-    const [_isLoading, _setIsLoading] = useState(true),
+    const [_data, _setData] = useState<Movie[] | null>(null),
+        [_isLoading, _setIsLoading] = useState(true),
         [_error, _setError] = useState<any>(null)
 
-    useEffect
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await api.get<Movie[]>("movies", {
+                    disableCache
+                })
+                _setData(response)
+            } catch(error) {
+                _setError(error)
+            } finally {
+                _setIsLoading(false)
+            }
+        }
+
+        fetchData()
+    }, [api, disableCache])
+
+    return [_data, {
+        isLoading: _isLoading,
+        isError: !!_error,
+        error: _error
+    }]
 }
