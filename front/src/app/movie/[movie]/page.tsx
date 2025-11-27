da7f010f56cb6de790d19ee20b4f50b6b6f520e7
Lautaro <lautaro.kaza@gmail.com>
2025-11-27 17:48:39 -0300

    Add opinion card component

--- front/src/app/movie/[movie]/page.tsx
+++ front/src/app/movie/[movie]/page.tsx
@@ -1,4 +1,5 @@
 import MovieData from "@/components/MovieData";
+import OpinionCard from "@/components/OpinionCard";
 import { useApi } from "@/hooks/api";
 import { Movie } from "@/types/movie";
 import { redirect } from "next/navigation";
@@ -20,6 +21,14 @@ export default async function MoviePage({
                     <MovieData
                         movie={movie}
                     />
+                </section>
+                <section className="py-10 container-x-padding flex flex-col gap-10">
+                    <h2 className="text-xl font-bold">Opiniones</h2>
+                    <div className="w-full h-fit flex flex-col gap-5">
+                        <OpinionCard />
+                        <OpinionCard />
+                        <OpinionCard />
+                    </div>
                 </section>
             </main>
         )
---
+++ front/src/components/OpinionCard.tsx
@@ -0,0 +1,17 @@
+import Avatar from "./Avatar";
+import Rating from "./Rating";
+
+export default function OpinionCard() {
+    return (
+        <article className="w-full h-fit p-7 flex flex-col gap-5 rounded-xl border-[var(--color-border)] border-1">
+            <div className="w-full h-fit flex items-start gap-5 justify-between">
+                <div className="w-fit h-fit flex items-center gap-3">
+                    <Avatar>Lautaro</Avatar>
+                    <span className="text-md font-semibold">Lautaro Kazalukian</span>
+                </div>
+                <Rating value="2.5" />
+            </div>
+            <p>Esta es una reseña :D</p>
+        </article>
+    )
+}
