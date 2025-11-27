e6c4367ba6fb3558faec963cd2cf6338707fcc3d
Lautaro <lautaro.kaza@gmail.com>
2025-11-27 02:51:27 -0300

    Add user button component

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,36 +1,26 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// componentes/Navbar.tsx
 "use client";
 
 import Link from "next/link";
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import KinoLogo from "@/../public/logo.png";
-import Avatar from "./Avatar";
+import { User } from "@/types/user";
+import UserButton from "./UserButton";
 
 export default function Navbar() {
   // Simulacion del estado de autenticacion
   const [isLoggedIn, setIsLoggedIn] = useState(true);
-  const [openMenu, setOpenMenu] = useState(false);
 
   const menuRef = useRef<HTMLDivElement>(null);
 
   // Datos del usuario (mock)
-  const user = {
+  const user: User = {
+    id: "uuid",
     name: "Valentin Fortunato",
     email: "gmailprueba@gmail.com",
-    initials: "VF",
+    address: "123 av siempreviva",
+    roles: "admin"
   };
-
-  useEffect(() => {
-    const handleClick = (e: MouseEvent) => {
-      if(menuRef.current && !menuRef.current.contains(e.target as Node)) {
-        setOpenMenu(false);
-      }
-    };
-    document.addEventListener("mousedown", handleClick);
-    return () => document.removeEventListener("mousedown", handleClick);
-  }, []);
 
   return (
     <nav className="bg-[var(--background)]/50 backdrop-blur-xl relative z-[9999] sticky top-0 z-50 container-x-padding py-4 flex items-center justify-between">
@@ -62,7 +52,7 @@ export default function Navbar() {
           </Link>
           <Link
             href="/register"
-            className="bg-[#F3CC63] hover:bg-[#f6d783] text-black font-semibold px-6 py-2 rounded-full transition"
+            className="bg-[var(--color-primary)] hover:bg-[var(--foreground)] hover:text-[var(--background)] text-[var(--primary-foreground)] font-semibold px-6 py-3 rounded-full transition-colors duration-200"
           >
             Crear cuenta
           </Link>
@@ -83,39 +73,9 @@ export default function Navbar() {
           </Link>
 
           {/* Avatar con dropdown (opcional) */}
-          <div className="grid grid-cols-[auto_minmax(0,1fr)] items-center gap-3 cursor-pointer group rounded-full bg-white/10 p-2 pr-6 max-w-58 select-none"
-          ref={menuRef}
-          onClick={() => setOpenMenu(!openMenu)}
-          >
-            <Avatar>{user.name}</Avatar>
-            <div className="text-white max-w-full">
-              <p className="font-semibold w-full whitespace-nowrap text-ellipsis overflow-hidden text-md">{user.name}</p>
-              <p className="text-sm text-gray-400 w-full whitespace-nowrap text-ellipsis overflow-hidden">{user.email}</p>
-            </div>
-
-            {/* Menu desplegable*/}
-            {openMenu && (
-              <div className="absolute right-0 top-16 w-48 bg-[#232323] border border-gray-700 rounded-xl shadow-xl p-2">
-                <Link
-                  href="/perfil"
-                  className="block px-4 py-2 text-white hover:bg-gray-700 rounded-lg"
-                >
-                  Mi perfil
-                </Link>
-                <Link
-                  href="/reservas"
-                  className="block px-4 py-2 text-white hover:bg-gray-700 rounded-lg"
-                >
-                  Mis reservas
-                </Link>
-                <button
-                  className="w-full text-left px-4 py-2 text-red-400 hover:bg-gray-700 rounded-lg cursor-pointer"
-                >
-                  Cerrar sesión
-                </button>
-              </div>
-            )}
-          </div>
+          <UserButton
+            user={user}
+          />
         </div>
       )}
     </nav>
---
+++ front/src/components/UserButton.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { User } from "@/types/user"
+import Avatar from "./Avatar"
+import { useState } from "react"
+import Link from "next/link"
+
+export default function UserButton({
+    user
+}: {
+    user: User
+}) {
+    const [_menuOpen, _setMenuOpen] = useState(false)
+
+    return (
+        <div className="w-fit h-fit relative">
+            <button onClick={() => _setMenuOpen(!_menuOpen)} className="grid grid-cols-[auto_minmax(0,1fr)] items-center gap-3 cursor-pointer group rounded-full bg-white/10 p-2 pr-6 max-w-58 select-none">
+                <Avatar>{user.name}</Avatar>
+                <div className="text-white max-w-full">
+                <p className="font-semibold w-full whitespace-nowrap text-ellipsis overflow-hidden text-md">{user.name}</p>
+                <p className="text-sm text-gray-400 w-full whitespace-nowrap text-ellipsis overflow-hidden">{user.email}</p>
+                </div>
+            </button>
+            {
+                _menuOpen && (
+                    <div className="absolute right-0 top-16 w-48 bg-[#232323] border border-gray-700 rounded-xl shadow-xl p-2">
+                        <Link
+                            href="/perfil"
+                            className="block px-4 py-2 text-white hover:bg-gray-700 rounded-lg"
+                        >
+                            Mi perfil
+                        </Link>
+                        <Link
+                            href="/reservas"
+                            className="block px-4 py-2 text-white hover:bg-gray-700 rounded-lg"
+                        >
+                            Mis reservas
+                        </Link>
+                        <button
+                            className="w-full text-left px-4 py-2 text-red-400 hover:bg-gray-700 rounded-lg cursor-pointer"
+                        >
+                            Cerrar sesión
+                        </button>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
