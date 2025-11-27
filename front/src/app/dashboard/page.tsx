40ec513cb61629166f3c73f670aab653a918e89c
Lautaro <lautaro.kaza@gmail.com>
2025-11-27 18:36:42 -0300

    Add dashboard page

---
+++ front/src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/Footer";
+
+export default function Dashboard() {
+    return (
+        <>
+            <main>
+                <section className="container-x-padding py-10">
+                    <h1 className="text-3xl font-bold">Bienvenido, <span className="text-[var(--color-primary)]">Valentin</span></h1>
+                </section>
+            </main>
+            <Footer />
+        </>
+    )
+}
