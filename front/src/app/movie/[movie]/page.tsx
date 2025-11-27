b86b77db578d75ff47248e50e0d744d982ff3671
Lautaro <lautaro.kaza@gmail.com>
2025-11-27 17:15:07 -0300

    Implement movie data component

--- front/src/app/movie/[movie]/page.tsx
+++ front/src/app/movie/[movie]/page.tsx
@@ -1,5 +1,29 @@
-export default async function MoviePage() {
-    return (
-        <h1>Movie</h1>
-    )
+import MovieData from "@/components/MovieData";
+import { useApi } from "@/hooks/api";
+import { redirect } from "next/navigation";
+
+export default async function MoviePage({
+    params
+}: {
+    params: Promise<{
+        movie: string
+    }>
+}) {
+    try {
+        const api = useApi(),
+            movieD = await api.get<any>(`movies/${(await params).movie}`),
+            movie = {...movieD, synopsis: movieD.sinopsis}
+
+        return (
+            <main>
+                <section className="py-10 container-x-padding h-[550px]">
+                    <MovieData
+                        movie={movie}
+                    />
+                </section>
+            </main>
+        )
+    } catch {
+        redirect("/")
+    }
 }
