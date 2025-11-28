2930e5a48f514a17c2ea08ae0671ba41a487af5f
Valentin Fortunato <valenfortunato06@gmail.com>
2025-11-28 18:29:19 -0300

    Rama Valentin: seguimiento

--- front/src/context/authContext.tsx
+++ front/src/context/authContext.tsx
@@ -23,7 +23,6 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({children}) => {
   const [dataUser, setDataUser] = useState<userSessionInterface | null>(null);
 
   useEffect(() => {
-    console.log("dataUser CAMBIÓ →", dataUser);
     if(dataUser){
         localStorage.setItem('userSession', JSON.stringify(dataUser))
     }
