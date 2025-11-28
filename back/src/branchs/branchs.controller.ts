d603fb11211fbdbd404a761bdb144da09a66acd9
FranciscoArenas20 <franciscoarenas.2094@gmail.com>
2025-11-27 20:38:18 -0400

    swagger doc first part

--- back/src/branchs/branchs.controller.ts
+++ back/src/branchs/branchs.controller.ts
@@ -1,39 +1,48 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, Query } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, Query, Patch } from '@nestjs/common';
 import { BranchService } from './branchs.service';
 import { Branch } from './branch.entity';
 import { CreateBranchDto } from './dto/create-branch.dto';
-import { UpdateBranchDto } from './dto/update-branch.dto';@Controller('branches')
+import { UpdateBranchDto } from './dto/update-branch.dto';import { ApiOperation, ApiTags } from '@nestjs/swagger';
+@Controller('branches')
 
-
+@ApiTags('branches (Sucursales)')
 @Controller('branches')
 export class BranchController {
   constructor(private readonly branchService: BranchService) {}
 
+  @ApiOperation({ summary: 'Obtener todas las sucursales registradas' })
   @Get()
   findAll(): Promise<Branch[]> {
     return this.branchService.findAll();
   }
 
+  @ApiOperation({ summary: 'Obtener sucursales por busqueda parcial de su dirección',
+    description:'Este endpoint retorna una o mas sucursales que coincidan total o parcialmente entre su dirección registrada y la busqueda realizada'
+   })
   @Get('search')
   findByPartialAddress(@Query('q') search: string): Promise<Branch[]> {
     return this.branchService.findBranchesByPartialAddress(search);
   }
 
+  @ApiOperation({ summary: 'Obtener una sucursal por su UUID' })
   @Get(':id')
   findById(@Param('id') id: string): Promise<Branch> {
     return this.branchService.findById(id);
   }
 
+  @ApiOperation({ summary: 'Registrar una nueva sucursal' })
   @Post()
   createBranch(@Body() dto: CreateBranchDto): Promise<Branch> {
     return this.branchService.createBranch(dto);
   }
 
-  @Put(':id')
+  @ApiOperation({ summary: 'Actualizar una o varias propiedades de una sucursal registrada' })
+  @Patch(':id')
   updateBranch(@Param('id') id: string, @Body() dto: UpdateBranchDto): Promise<Branch> {
     return this.branchService.updateBranch(id, dto);
   }
 
+  @ApiOperation({ summary: 'Eliminar una sucursal a traves de su UUID' })
   @Delete(':id')
   deleteBranch(@Param('id') id: string): Promise<string> {
     return this.branchService.deleteBranch(id)
--- back/src/branchs/dto/create-branch.dto.ts
+++ back/src/branchs/dto/create-branch.dto.ts
@@ -1,22 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsOptional, IsNumber, MaxLength } from 'class-validator';
 
 export class CreateBranchDto {
+
+  @ApiProperty({
+    example: 'Kino del Valle',
+    description: 'Nombre de la sucursal',
+  })
   @IsString()
   @IsNotEmpty()
   @MaxLength(150)
   name: string;
 
+  @ApiProperty({
+    example: 'Av. Principal 123, Centro Comercial MundoShop, Ciudad de México',
+    description: 'Dirección completa de la sucursal.',
+  })
   @IsString()
   @IsNotEmpty()
   @MaxLength(255)
   address: string;
 
+  @ApiProperty({
+    example: 19.432608,
+    description: 'Latitud geográfica de la sucursal.',
+  })
   @IsNumber()
   latitude: number;
 
+  @ApiProperty({
+    example: -99.133209,
+    description: 'Longitud geográfica de la sucursal.',
+  })
   @IsNumber()
   longitude: number;
 
+  @ApiProperty({
+    example: 'ChIJU4MpT9i_0YURgQdX1bw5Q1g',
+    description: 'ID del lugar en Google Places (opcional).',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   googlePlaceId?: string;
