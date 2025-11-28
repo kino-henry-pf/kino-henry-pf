8f0e0400f98e99a307546bb1a8929bffb06ec730
FranciscoArenas20 <franciscoarenas.2094@gmail.com>
2025-11-28 02:06:46 -0400

    Swagger implementation

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,21 +1,26 @@
 import { Controller, Get, HttpCode, Param, Patch } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './entity/user.entity';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users (Usuarios)')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Obtener Todos los usuarios'})
   @Get()
   async findAll(): Promise<User[]> {
     return await this.usersService.findAll();
   }
 
+  @ApiOperation({ summary: 'Obtener usuario mediante UUID'})
   @Get(':id')
   async findById(@Param('id') id: string): Promise<User> {
     return await this.usersService.findById(id);
   }
 
+  @ApiOperation({ summary: 'Eliminar usuario mediante UUID'})
   @Patch('/delete/:id')
   @HttpCode(204)
   async deleteUser(@Param('id') id: string) {
