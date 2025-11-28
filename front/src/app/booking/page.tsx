769d3f1eea3e4ad447ee0b935f81cc249c3ad298
Lautaro <lautaro.kaza@gmail.com>
2025-11-28 19:49:48 -0300

    Add movie selector component

--- front/src/app/booking/page.tsx
+++ front/src/app/booking/page.tsx
@@ -1,7 +1,42 @@
-export default function BookingPage() {
+import Button from "@/components/Button";
+import Footer from "@/components/Footer";
+import MovieSelector from "@/components/MovieSelector";
+import { useApi } from "@/hooks/api";
+import { Movie } from "@/types/movie";
+import * as Icon from "akar-icons"
+
+export default async function BookingPage() {
+    const api = useApi(),
+        movies = await api.get<Movie[]>("movies")
+
     return (
-        <main>
-            <h1>Hola :D</h1>
-        </main>
+        <>
+            <section className="pt-10 flex flex-col items-center gap-10">
+                <div className="w-fit h-fit flex flex-col gap-20">
+                    <div className="container-x-padding w-[900px] max-w-full h-fit flex flex-col gap-2">
+                        <h1 className="text-md font-bold text-center">&iquest;Qué películas vas a ver?</h1>
+                        <p className="text-3xl font-bold text-center">Elige las películas que quieres ver</p>
+                    </div>
+                    <div className="container-x-padding w-[900px] max-w-full grid lg:grid-cols-4 grid-cols-2 h-fit gap-8 lg:gap-7">
+                        {
+                            movies.map(movie => (
+                                <MovieSelector
+                                    key={movie.id}
+                                    movie={movie}
+                                />
+                            ))
+                        }
+                    </div>
+                </div>
+                <nav className="w-full h-20 sticky bottom-0 left-0 bg-black/90 backdrop-blur-sm flex items-center justify-end gap-10 container-x-padding">
+                    <span className="text-md font-semibold">Total: $10.000,00</span>
+                    <Button rounded>
+                        <span>Siguiente</span>
+                        <Icon.ArrowRight className="size-4" strokeWidth={3} />
+                    </Button>
+                </nav>
+            </section>
+            <Footer />
+        </>
     )
 }
--- front/src/components/Button.tsx
+++ front/src/components/Button.tsx
@@ -4,14 +4,14 @@ export default function Button({
     primary = true,
     onClick
 }: {
-    children: string,
+    children: React.ReactNode,
     rounded?: boolean,
     primary?: boolean,
     onClick?: () => any
 }) {
     return (
         <button onClick={onClick} className={[
-            "w-fit h-fit cursor-pointer py-3 px-6 font-bold text-sm",
+            "w-fit h-fit cursor-pointer py-3 px-6 font-bold text-sm flex items-center gap-2",
             rounded ? "rounded-full" : "rounded-xl",
             primary ? "bg-[var(--color-primary)] text-[var(--primary-foreground)]" : "bg-white/30"
         ].join(" ")}>
