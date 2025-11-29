c59a7c8398a066327441edf82e9c0932eb3dd7e5
FranciscoArenas20 <franciscoarenas.2094@gmail.com>
2025-11-28 22:15:02 -0400

    supabase without finished

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, HttpCode, Param, Patch } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Patch, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './entity/user.entity';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/decorator/role.decorator';
+//import { AuthGuard } from 'src/auth/guards/auth-guard.guard';
+//import { RolesGuard } from 'src/auth/guards/role-guard.guard';
 
 @ApiTags('users (Usuarios)')
 @Controller('users')
@@ -9,13 +12,18 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @ApiOperation({ summary: 'Obtener Todos los usuarios'})
+  @ApiBearerAuth()
   @Get()
+  // @Roles('admin')
+  // @UseGuards(AuthGuard, RolesGuard)
   async findAll(): Promise<User[]> {
     return await this.usersService.findAll();
   }
 
   @ApiOperation({ summary: 'Obtener usuario mediante UUID'})
+  @ApiBearerAuth()
   @Get(':id')
+  // @UseGuards(AuthGuard)
   async findById(@Param('id') id: string): Promise<User> {
     return await this.usersService.findById(id);
   }
