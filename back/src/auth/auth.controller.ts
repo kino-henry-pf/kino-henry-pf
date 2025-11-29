872bbcbf70f2b3e4b61fbbdff389533896586bcf
FranciscoArenas20 <franciscoarenas.2094@gmail.com>
2025-11-29 00:14:18 -0400

    fix bug

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -19,18 +19,18 @@ export class AuthController {
     return this.authService.login(dto);
   }
 
-  @Get('login') 
-  async login(@Query('provider') provider: string, @Res() res) { 
-    const { data, error } = await supabase.auth.signInWithOAuth({ 
-      provider: "google",
-      options: { redirectTo: 'http://localhost:3000/auth/callback', 
-  }, 
-}); if (error) return res.status(400).json(error); return res.redirect(data.url); } 
+//   @Get('login') 
+//   async login(@Query('provider') provider: string, @Res() res) { 
+//     const { data, error } = await supabase.auth.signInWithOAuth({ 
+//       provider: "google",
+//       options: { redirectTo: 'http://localhost:3000/auth/callback', 
+//   }, 
+// }); if (error) return res.status(400).json(error); return res.redirect(data.url); } 
 
-@Get('callback') 
-async callback(@Query('code') code: string) { 
-  const { data, error } = await supabase.auth.exchangeCodeForSession(code); 
-  if (error) throw error; // data contains user + access_token + refresh_token 
-  return data; 
-} }
-
+// @Get('callback') 
+// async callback(@Query('code') code: string) { 
+//   const { data, error } = await supabase.auth.exchangeCodeForSession(code); 
+//   if (error) throw error; // data contains user + access_token + refresh_token 
+//   return data; 
+// } }}
+}
