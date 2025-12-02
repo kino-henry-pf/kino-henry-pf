633f121633d84e35722fe74b2c1fbf7750963156
Lautaro <lautaro.kaza@gmail.com>
2025-12-02 03:03:42 -0300

    Add responsive

--- front/src/app/products/[product]/page.tsx
+++ front/src/app/products/[product]/page.tsx
@@ -21,16 +21,17 @@ export default async function ProductPage({
         return (
             <>
                 <main className="w-full h-fit min-h-[calc(100vh-6rem)]">
-                    <section className="w-full h-full grid grid-cols-[minmax(auto,.5fr)_1fr] container-x-padding py-10 grid-rows-[500px] items-center gap-10">
-                        <div className="w-full h-full relative">
+                    <section className="w-full h-full grid grid-cols-1 lg:grid-cols-[minmax(auto,.5fr)_1fr] container-x-padding py-10 grid-rows-[350px_auto] lg:grid-rows-[500px] items-center gap-10">
+                        <div className="w-full h-full relative bg-[#f2f2f2]">
                             <Image
                                 alt={product.name}
                                 src={product.image}
                                 fill
+                                className="object-contain"
                             />
                         </div>
                         <div className="w-full h-fit flex flex-col gap-2">
-                            <h1 className="font-bold text-6xl">{product.name}</h1>
+                            <h1 className="font-bold text-6xl mb-4 lg:mb-0">{product.name}</h1>
                             <p>{product.description}</p>
                             <p className="font-semibold text-xl text-[var(--color-primary)]">{
                                 parseFloat(product.price).toLocaleString("es-AR", {
