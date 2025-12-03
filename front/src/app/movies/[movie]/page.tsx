7cb6a6506b334eadddabbc539676ef8be7af54a2
Hekimianz <guerrerohek121@gmail.com>
2025-12-03 17:33:55 -0600

    fix: unwrapped params.movie

--- front/src/app/movies/[movie]/page.tsx
+++ front/src/app/movies/[movie]/page.tsx
@@ -1,44 +1,44 @@
-import Footer from "@/components/Footer";
-import MovieData from "@/components/MovieData";
-import OpinionCard from "@/components/OpinionCard";
-import { apiClient } from "@/services/apiClient";
-import { Movie } from "@/types/movie";
-import { redirect } from "next/navigation";
+import Footer from '@/components/Footer';
+import MovieData from '@/components/MovieData';
+import OpinionCard from '@/components/OpinionCard';
+import { apiClient } from '@/services/apiClient';
+import { Movie } from '@/types/movie';
+import { redirect } from 'next/navigation';
 
 export default async function MoviePage({
-    params
+  params,
 }: {
-    params: Promise<{
-        movie: string
-    }>
+  params: Promise<{ movie: string }> | { movie: string };
 }) {
-    try {
-        const api = apiClient(),
-            movie = await api.get<Movie>(`movies/${(await params).movie}`, {
-                disableCache: true
-            })
+  try {
+    const { movie } = await params;
 
-        return (
-            <>
-                <main>
-                    <section className="py-10 container-x-padding h-[550px]">
-                        <MovieData
-                            movie={movie}
-                        />
-                    </section>
-                    <section className="py-10 container-x-padding flex flex-col gap-10">
-                        <h2 className="text-xl font-bold">Opiniones</h2>
-                        <div className="w-full h-fit flex flex-col gap-5">
-                            <OpinionCard />
-                            <OpinionCard />
-                            <OpinionCard />
-                        </div>
-                    </section>
-                </main>
-                <Footer />
-            </>
-        )
-    } catch (error) {
-        redirect("/")
-    }
+    const api = apiClient();
+    const movieData = await api.get<Movie>(`movies/${movie}`, {
+      disableCache: true,
+    });
+
+    return (
+      <>
+        <main>
+          <section className="py-10 container-x-padding h-[550px]">
+            <MovieData movie={movieData} />
+          </section>
+
+          <section className="py-10 container-x-padding flex flex-col gap-10">
+            <h2 className="text-xl font-bold">Opiniones</h2>
+            <div className="w-full h-fit flex flex-col gap-5">
+              <OpinionCard />
+              <OpinionCard />
+              <OpinionCard />
+            </div>
+          </section>
+        </main>
+
+        <Footer />
+      </>
+    );
+  } catch (error) {
+    redirect('/');
+  }
 }
