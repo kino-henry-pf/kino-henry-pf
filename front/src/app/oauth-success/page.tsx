8296805856c8729c0bbcd6a4272b196e4766129d
Valentin Fortunato <valenfortunato06@gmail.com>
2025-12-05 03:18:50 -0300

    Rama Vercel

--- front/src/app/oauth-success/page.tsx
+++ front/src/app/oauth-success/page.tsx
@@ -1,8 +1,37 @@
-import React from "react";
-import OauthSuccessPage from "./oauthSuccess";
+"use client";
 
-export default function LoginGoogle () {
-    return(
-      <OauthSuccessPage token={null} />
-    );
+import { useEffect } from "react";
+import { useAuth } from "@/context/authContext";
+
+export default function OauthSuccessPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const { setDataUser } = useAuth();
+  const token = searchParams.token;
+
+  useEffect(() => {
+    console.log("TOKEN RECIBIDO DESDE URL:", token);
+
+    if (!token) return;
+
+    const session = {
+      message: true,
+      access_token: token,
+      user: {
+        id: "",
+        name: "",
+        email: "",
+        role: "user",
+      },
+    };
+
+    localStorage.setItem("userSession", JSON.stringify(session));
+    setDataUser(session);
+
+    window.location.href = "/";
+  }, [token]);
+
+  return <p>Procesando login...</p>;
 }
