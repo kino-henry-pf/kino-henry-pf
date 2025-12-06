784f052beceb384296e894b2b9f632973ae14581
Valentin Fortunato <valenfortunato06@gmail.com>
2025-12-05 21:40:17 -0300

    Rama MyProfile

---
+++ front/src/components/Profile.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import { useAuth } from "@/context/authContext"
+
+export default function MyProfilePage() {
+  const { dataUser } = useAuth();
+
+  const mockOrders = [
+    {
+      id: 1,
+      date: "Viernes 3 de Noviembre",
+      movie: "Toy Story",
+      status: "Confirmada"
+    },
+    {
+      id: 2,
+      date: "Sábado 11 de Noviembre",
+      movie: "Avengers",
+      status: "Confirmada"
+    },
+    {
+      id: 3,
+      date: "Domingo 19 de Noviembre",
+      movie: "Interstellar",
+      status: "Cancelada"
+    },
+  ]
+
+  return (
+    <div className="min-h-screen bg-[#0F0F0F] text-white p-6 pt-10">
+
+      <h1 className="text-4xl font-bold mb-2">
+        Mi <span className="text-yellow-500">Perfil</span>
+      </h1>
+      <p className="text-gray-400 mb-10">Información de tu cuenta</p>
+
+      <div className="bg-[#1A1A1A] p-6 rounded-2xl max-w-xl shadow-lg border border-[#2a2a2a] mb-12">
+
+        <div className="mb-6">
+          <p className="text-gray-400 text-sm">Nombre</p>
+          <p className="text-xl font-semibold">
+            {dataUser?.user.name || "Cargando..."}
+          </p>
+        </div>
+
+        <div className="mb-6">
+          <p className="text-gray-400 text-sm">Email</p>
+          <p className="text-xl font-semibold">
+            {dataUser?.user.email || "Cargando..."}
+          </p>
+        </div>
+      </div>
+
+      <h2 className="text-2xl font-semibold mb-4">Historial de órdenes</h2>
+
+      <div className="space-y-4 max-w-3xl">
+
+        {mockOrders.map(order => (
+          <div
+            key={order.id}
+            className="bg-[#1A1A1A] border border-[#2a2a2a] rounded-xl px-6 py-4 flex justify-between items-center shadow-md"
+          >
+            <div>
+              <p className="font-semibold text-lg">{order.date}</p>
+              <p className="text-yellow-500 font-bold text-md mt-1">
+                ● {order.movie}
+              </p>
+            </div>
+
+            <p
+              className={`text-sm px-3 py-1 rounded-full border 
+                ${order.status === "Confirmada"
+                  ? "border-green-600 text-green-400"
+                  : "border-red-600 text-red-400"
+                }`}
+            >
+              {order.status}
+            </p>
+          </div>
+        ))}
+
+      </div>
+    </div>
+  )
+}
