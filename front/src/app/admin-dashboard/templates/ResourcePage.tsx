49a93190afab33c8484d8372a80ffdc3d07d86f9
Lautaro <lautaro.kaza@gmail.com>
2025-12-07 04:23:20 -0300

    Add resource templates

---
+++ front/src/app/admin-dashboard/templates/ResourcePage.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { useQuery } from "@/hooks/useQuery"
+import DataTable from "@/components/DataTable"
+import { useEffect, useRef } from "react"
+
+export default function ResourcePage<T>({
+    resource,
+    title,
+    head,
+    mapRow
+}: {
+    resource: string,
+    title: string,
+    head: string[],
+    mapRow: (item: T) => {
+        resourceId: string,
+        value: (string | React.ReactNode)[]
+    }
+}) {
+    const query = useQuery<T[]>(resource),
+        topSectionRef = useRef<HTMLDivElement | null>(null)
+
+    useEffect(() => {
+        if (!topSectionRef.current) return
+        const scrollTop = window.scrollY + topSectionRef.current.getBoundingClientRect().top
+        window.scrollTo({top: scrollTop - 133.33, behavior: "smooth"})
+    }, [topSectionRef, resource])
+
+    return (
+        <section ref={topSectionRef} className="w-full h-fit flex flex-col gap-3">
+            {
+                query.data ? (
+                    <>
+                        <p className="lg:![padding-right:0] lg:![padding-left:0] container-x-padding">{title} <span className="opacity-50">{!query.isLoading && query.data ? `(${query.data.length})` : null}</span></p>
+                        <DataTable
+                            resource={resource}
+                            head={head}
+                            body={query.data.map(mapRow)}
+                        />
+                    </>
+                ) : query.isLoading ? (
+                    <span>Cargando...</span>
+                ) : query.error ? (
+                    <span>ERROR</span>
+                ) : null
+            }
+        </section>
+    )
+}
---
+++ front/src/app/admin-dashboard/templates/UpsertResourcePage.tsx
@@ -0,0 +1,144 @@
+"use client"
+
+import { Form, Formik } from "formik"
+import { useEffect, useMemo, useRef, useState } from "react"
+import Button from "@/components/Button"
+import useMutation from "@/hooks/useMutation"
+import AlertModal from "@/components/AlertModal"
+import { FormField } from "../types"
+import Field from "../components/Field"
+import { useQuery } from "@/hooks/useQuery"
+import { useRouter } from "next/navigation"
+
+export default function UpsertResourcePage<T>({
+    resource,
+    type,
+    title,
+    submitText,
+    successMessage,
+    fields,
+    getterResource,
+    successRedirect,
+    mapPreview,
+    validate,
+    mapError
+}: {
+    resource: string,
+    type: "POST" | "PUT" | "PATCH" | "DELETE",
+    title: string,
+    submitText: string,
+    successMessage: string,
+    fields: FormField[],
+    getterResource?: string,
+    successRedirect?: (query: T) => string,
+    mapPreview?: (field: T) => string,
+    validate?: (values: any) => any,
+    mapError: (error: any) => {
+        title: string,
+        description: string
+    }
+}) {
+    const router = useRouter()
+
+    const topSectionRef = useRef<HTMLFormElement | null>(null)
+
+    const mutation = useMutation<any>(resource, {
+        type,
+        withFiles: fields.some(field => field.as === "file")
+    })
+
+    const query = getterResource ? useQuery<T>(getterResource) : null
+
+    const [_showError, _setShowError] = useState(false),
+        [_showSuccess, _setShowSuccess] = useState(false)
+
+    const errorData = useMemo(() => {
+        return mapError(mutation.error)
+    }, [mutation.error, mapError])
+
+    useEffect(() => {
+        if (!topSectionRef.current) return
+        const scrollTop = window.scrollY + topSectionRef.current.getBoundingClientRect().top
+        window.scrollTo({top: scrollTop - 300.33, behavior: "smooth"})
+    }, [topSectionRef])
+
+    useEffect(() => {
+        if (mutation.error) {
+            _setShowError(true)
+        }
+    }, [mutation.error])
+
+    useEffect(() => {
+        if (mutation.data) {
+            _setShowSuccess(true)
+        }
+    }, [mutation.data])
+
+    return (!getterResource || (getterResource && query?.data)) && (
+        <>
+            <Formik
+                validateOnMount
+                initialValues={
+                    Object.fromEntries(
+                        fields.map(field => [
+                            field.name,
+                            field.as === "select" ? field.options?.[0].value ?? "" : field.as !== "file" ? query?.data?.[field.name as keyof T] ?? "" : ""
+                        ])
+                    )
+                }
+                onSubmit={body => mutation.submit(body)}
+                validate={validate}
+                ref={topSectionRef}
+            >
+                {
+                    ({isValid}) => (
+                        <Form className="w-full h-fit flex flex-col gap-10 container-x-padding lg:![padding-left:0] lg:![padding-right:0] pb-5">
+                            <p>{title}</p>
+                            <div className="w-full h-fit flex flex-col gap-5">
+                                {
+                                    fields.map((field, index) => {
+                                        const preview = query?.data ? (mapPreview?.(query.data)) : undefined
+                                        return <Field disabled={field.isLoading || field.disabled || query?.isLoading || mutation.isLoading} preview={preview} {...field } key={index} />
+                                    })
+                                }
+                                <div className="w-full lg:w-[66.6%] lg:ml-auto h-12 flex justify-end pt-4 xl:pt-2">
+                                    <Button
+                                        width="100%"
+                                        type="submit"
+                                        loading={mutation.isLoading}
+                                        disabled={!isValid}
+                                    >
+                                        {submitText}
+                                    </Button>
+                                </div>
+                            </div>
+                        </Form>
+                    )
+                }
+            </Formik>
+            <AlertModal
+                show={_showError}
+                onClose={() => {
+                    _setShowError(false)
+                }}
+                title={errorData.title}
+                description={errorData.description}
+                icon="CircleAlert"
+                shortTitle="Error"
+            />
+            <AlertModal
+                show={_showSuccess}
+                onClose={() => {
+                    if (successRedirect && query?.data) {
+                        router.push(successRedirect(query.data))
+                    } else {
+                        _setShowSuccess(false)
+                    }
+                }}
+                title={successMessage}
+                icon="CircleCheck"
+                shortTitle="Operación exitosa"
+            />
+        </>
+    )
+}
