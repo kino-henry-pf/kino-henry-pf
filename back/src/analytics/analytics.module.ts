de8c647d994f0a60efe55ef2697a739198c3b1d4
Hekimianz <guerrerohek121@gmail.com>
2025-12-09 19:46:13 -0600

    Implemented route for getting all rooms occupancy status

--- back/src/analytics/analytics.module.ts
+++ back/src/analytics/analytics.module.ts
@@ -4,11 +4,13 @@ import { AnalyticsController } from './analytics.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from 'src/orders/entities/order.entity';
 import OrderDetail from 'src/orders/entities/orderDetails.entity';
+import Room from 'src/rooms/rooms.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Order]),
     TypeOrmModule.forFeature([OrderDetail]),
+    TypeOrmModule.forFeature([Room]),
   ],
   controllers: [AnalyticsController],
   providers: [AnalyticsService],
