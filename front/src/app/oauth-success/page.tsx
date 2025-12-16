c2da0dbfbaf324028ed7baa70e2baf39fa145ed4
Lautaro <lautaro.kaza@gmail.com>
2025-12-16 18:52:17 -0300

    Fix design bugs

--- front/src/app/oauth-success/page.tsx
+++ front/src/app/oauth-success/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { Suspense } from "react";
 import OAuthSuccessPage from "@/components/OauthSuccess";
 
 export default function LoginGoogle() {
   return (
-    <Suspense fallback={<p>Processing login...</p>}>
-      <OAuthSuccessPage />
-    </Suspense>
+    <OAuthSuccessPage />
   );
 }
