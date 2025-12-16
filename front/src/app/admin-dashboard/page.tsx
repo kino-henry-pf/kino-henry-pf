2970bac0c4cdcc037c3fc777d74b95b60de8aa2c
Hekimianz <guerrerohek121@gmail.com>
2025-12-15 23:43:24 -0600

    Fixed analytics caching

--- front/src/app/admin-dashboard/page.tsx
+++ front/src/app/admin-dashboard/page.tsx
@@ -1,111 +1,127 @@
-"use client"
+'use client';
 
-import { useEffect } from "react";
-import DataCard from "./components/DataCard";
-import RoomCard from "./components/RoomCard";
-import { useQuery } from "@/hooks/useQuery";
-import Loader from "../../components/Loader";
+import { useEffect } from 'react';
+import DataCard from './components/DataCard';
+import RoomCard from './components/RoomCard';
+import { useQuery } from '@/hooks/useQuery';
+import Loader from '../../components/Loader';
 
 type RoomOccupancy = {
-    roomId: string,
-    roomName: string
-    branch: string
-    totalSeats: number
-    sold: number
-}
+  roomId: string;
+  roomName: string;
+  branch: string;
+  totalSeats: number;
+  sold: number;
+};
 
 export default function AdminDashboardPage() {
-    const productsQuery = useQuery<{totalProducts: number}>("analytics/total-products"),
-        ticketsQuery = useQuery<{totalTickets: number}>("analytics/total-tickets"),
-        revenueQuery = useQuery<{totalRevenue: number}>("analytics/revenue"),
-        roomsOccupancyQuery = useQuery<RoomOccupancy[]>("analytics/rooms/occupancy")
+  const productsQuery = useQuery<{ totalProducts: number }>(
+    'analytics/total-products',
+    { refetchInterval: 5000 }
+  );
 
-    useEffect(() => {
-        window.scrollTo({top: 0, behavior: "smooth"})
-    }, [])
+  const ticketsQuery = useQuery<{ totalTickets: number }>(
+    'analytics/total-tickets',
+    { refetchInterval: 5000 }
+  );
 
-    return productsQuery.data
-    && ticketsQuery.data
-    && revenueQuery.data
-    && roomsOccupancyQuery.data ? (
-        <>
-            <section className="w-full h-fit flex flex-col gap-3 lg:![padding-right:0] lg:![padding-left:0] container-x-padding">
-                <p>Statistics</p>
-                <div className="w-full grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-                    <DataCard
-                        icon="Glasses"
-                        value={
-                            productsQuery.data?.totalProducts.toString() || "-"
-                        }
-                        label="Products sold"
-                        color="cyan"
-                    />
-                    <DataCard
-                        icon="Ticket"
-                        value={
-                            ticketsQuery.data?.totalTickets.toString() || "-"
-                        }
-                        label="Tickets sold"
-                        color="orange"
-                    />
-                    <DataCard
-                        icon="Coin"
-                        value={
-                            revenueQuery.data?.totalRevenue.toLocaleString("es-AR", {
-                                style: "currency",
-                                currency: "ARS"
-                            }) || "-"
-                        }
-                        label="Total raised"
-                        color="purple"
-                    />
-                </div>
-            </section>
-            <section className="w-full h-fit flex flex-col gap-7 lg:![padding-right:0] lg:![padding-left:0] container-x-padding pb-5 xl:pb-0">
-                <p>Room capacity</p>
-                <div className="w-full flex flex-col gap-7">
-                    {
-                        roomsOccupancyQuery.data?.reduce((acc, curr) => {
-                            if (!acc.find(branch => branch.branch === curr.branch)) {
-                                acc.push({
-                                    branch: curr.branch,
-                                    occupancy: []
-                                })
-                            }
+  const revenueQuery = useQuery<{ totalRevenue: number }>('analytics/revenue', {
+    refetchInterval: 5000,
+  });
 
-                            const index = acc.findIndex(branch => branch.branch === curr.branch)
+  const roomsOccupancyQuery = useQuery<RoomOccupancy[]>(
+    'analytics/rooms/occupancy',
+    { refetchInterval: 5000 }
+  );
 
-                            acc[index].occupancy.push(curr)
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, []);
 
-                            return acc
-                        }, [] as {
-                            branch: string,
-                            occupancy: RoomOccupancy[]
-                        }[]).map(branch => (
-                            <div key={branch.branch} className="w-full h-fit flex flex-col gap-5">
-                                <p className="text-sm font-semibold">{branch.branch}</p>
-                                <div className="w-full h-fit flex flex-col gap-4">
-                                    {
-                                        branch.occupancy.map((room, index) => (
-                                            <RoomCard
-                                                key={index}
-                                                id={room.roomId}
-                                                name={room.roomName}
-                                                seats={room.totalSeats}
-                                                sold={room.sold}
-                                            />
-                                        ))
-                                    }
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
-            </section>
-        </>
-    ) : (
-        <div className="w-full h-[400px] flex items-center justify-center">
-            <Loader className="size-10" />
+  return productsQuery.data &&
+    ticketsQuery.data &&
+    revenueQuery.data &&
+    roomsOccupancyQuery.data ? (
+    <>
+      <section className="w-full h-fit flex flex-col gap-3 lg:![padding-right:0] lg:![padding-left:0] container-x-padding">
+        <p>Statistics</p>
+        <div className="w-full grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+          <DataCard
+            icon="Glasses"
+            value={productsQuery.data?.totalProducts.toString() || '-'}
+            label="Products sold"
+            color="cyan"
+          />
+          <DataCard
+            icon="Ticket"
+            value={ticketsQuery.data?.totalTickets.toString() || '-'}
+            label="Tickets sold"
+            color="orange"
+          />
+          <DataCard
+            icon="Coin"
+            value={
+              revenueQuery.data?.totalRevenue.toLocaleString('es-AR', {
+                style: 'currency',
+                currency: 'ARS',
+              }) || '-'
+            }
+            label="Total raised"
+            color="purple"
+          />
         </div>
-    )
+      </section>
+      <section className="w-full h-fit flex flex-col gap-7 lg:![padding-right:0] lg:![padding-left:0] container-x-padding pb-5 xl:pb-0">
+        <p>Room capacity</p>
+        <div className="w-full flex flex-col gap-7">
+          {roomsOccupancyQuery.data
+            ?.reduce(
+              (acc, curr) => {
+                if (!acc.find((branch) => branch.branch === curr.branch)) {
+                  acc.push({
+                    branch: curr.branch,
+                    occupancy: [],
+                  });
+                }
+
+                const index = acc.findIndex(
+                  (branch) => branch.branch === curr.branch
+                );
+
+                acc[index].occupancy.push(curr);
+
+                return acc;
+              },
+              [] as {
+                branch: string;
+                occupancy: RoomOccupancy[];
+              }[]
+            )
+            .map((branch) => (
+              <div
+                key={branch.branch}
+                className="w-full h-fit flex flex-col gap-5"
+              >
+                <p className="text-sm font-semibold">{branch.branch}</p>
+                <div className="w-full h-fit flex flex-col gap-4">
+                  {branch.occupancy.map((room, index) => (
+                    <RoomCard
+                      key={index}
+                      id={room.roomId}
+                      name={room.roomName}
+                      seats={room.totalSeats}
+                      sold={room.sold}
+                    />
+                  ))}
+                </div>
+              </div>
+            ))}
+        </div>
+      </section>
+    </>
+  ) : (
+    <div className="w-full h-[400px] flex items-center justify-center">
+      <Loader className="size-10" />
+    </div>
+  );
 }
